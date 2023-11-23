6455ff766215b46e0040c9b84f896c29a3066ab2
Gabriela <dev.gabrielaellen@gmail.com>
2023-11-23 14:01:56 -0300

    Usa o db.sync para sincronizar ao banco

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from "express";
 import router from "./routes/filmesRoutes.js";
+import db from "./db/db.js";
 
 let app = express();
+
+db.sync()
+  .then(() => {
+    console.log("Banco de dados sincronizado");
+  })
+  .catch((err) => {
+    console.log("Erro ao sincronizar o banco: " + err.message);
+  });
+
 app.use(express.json());
 
 app.use(router);
 
 app.listen(5000, ()=>{
-    console.log("Servidor excutando na porta 5000!");
+    console.log("Servidor executando na porta 5000!");
 });
